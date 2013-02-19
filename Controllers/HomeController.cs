856d06615cf18b2e2a7671dba981e8547eecf750
Zhang Xin <zhangxin840@gmail.com>
2013-02-19 23:27:59 +0800

    Change to MVC project, add AJAX proxy

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BaiduInMetro.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+
+            return Redirect("~/BaiduInMetro/getContent.html");
+        }
+        public string TestAction()
+        {
+            return "Test Action";
+            //return Redirect("~/BaiduInMetro/getContent.html");
+        }
+    }
+}
---
+++ Controllers/ProxyController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace BaiduInMetro.Controllers
+{
+    public class ProxyController : ApiController
+    {
+        // GET api/proxy
+        public string Get()
+        {
+            return "This is cross-domain AJAX proxy, request with URL to get resource.";
+        }
+
+        public string GetByUrl(string Url)
+        {
+            //string strURL = "http://www.baidu.com";
+            String strUrl = Url;
+            String result;
+            WebResponse objResponse;
+            WebRequest objRequest;
+            StreamReader sr;
+            
+            try {
+                objRequest = HttpWebRequest.Create(strUrl);
+                
+                // Send request and wait for response
+                objResponse = objRequest.GetResponse();
+
+                // Get charset name
+                string CharSet = "";
+                string strType = objResponse.Headers["Content-Type"].ToString();
+                if (!string.IsNullOrEmpty(strType))
+                {
+                    int strIndex = strType.IndexOf("charset=");
+                    if (strIndex > 0)
+                        CharSet = strType.Substring(strIndex + 8);
+                    else
+                        CharSet = "utf-8";
+                }
+
+                sr = new StreamReader(objResponse.GetResponseStream(), System.Text.Encoding.GetEncoding(CharSet));
+                result = sr.ReadToEnd();
+
+                sr.Close();
+                objResponse.Close();
+            }
+            catch (Exception ex){
+                return ex.Message;
+            }
+   
+            return result;
+        }
+
+    }
+}
